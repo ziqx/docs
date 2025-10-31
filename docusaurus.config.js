5435df05d4fd2e837141e09fca240829132c0950
Fathah KA <48355244+fathah@users.noreply.github.com>
2025-10-31 12:08:40 +0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,87 +1,86 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Ziqx Docs',
-  tagline: 'Ziqx Documentation',
-  url: 'https://docs.ziqx.in',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Ziqx', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  title: "Ziqx Docs",
+  tagline: "Ziqx Documentation",
+  url: "https://docs.ziqx.in",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Ziqx", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'Ziqx Docs',
+      title: "Ziqx Docs",
       logo: {
-        alt: 'Ziqx',
-        src: 'img/logo.svg',
+        alt: "Ziqx",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Tutorial',
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Tutorial",
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/Ziqx/docs',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/Ziqx/docs",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: "Tutorial",
+              to: "/docs/intro",
             },
           ],
         },
         {
-          title: 'Connect Us',
+          title: "Connect Us",
           items: [
-          
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/ziqx_cr',
+              label: "Twitter",
+              href: "https://twitter.com/ziqx_cr",
             },
             {
-              label: 'Facebook',
-              href: 'https://www.facebook.com/ziqx.cr/',
+              label: "Facebook",
+              href: "https://www.facebook.com/ziqx.cr/",
             },
             {
-              label: 'Instagram',
-              href: 'https://www.instagram.com/ziqx_cr',
+              label: "Instagram",
+              href: "https://www.instagram.com/ziqx.cc",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/ziqx_cr',
+              label: "Twitter",
+              href: "https://twitter.com/ziqx_cr",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Ziqx Home',
-              to: 'https://ziqx.in',
+              label: "Ziqx Home",
+              to: "https://ziqx.cc",
             },
             {
-              label: 'Blog',
-              to: '/blog',
+              label: "Blog",
+              to: "/blog",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/Ziqx',
+              label: "GitHub",
+              href: "https://github.com/ziqx",
             },
           ],
         },
@@ -95,22 +94,21 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/Ziqx/docs/tree/main/',
+          editUrl: "https://github.com/Ziqx/docs/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
